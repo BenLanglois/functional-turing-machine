d3dc6ba898a460c03a9b3c7f7990536d67ae19b6
Ben Langlois <blanglois000@gmail.com>
2018-07-25 21:10:20 -0400

    Implemented if, input, and print statements.

--- functional_turing_machine.py
+++ functional_turing_machine.py
@@ -79,6 +79,18 @@ class TuringTape:
 			' '.join(str(cell) for cell in self._tape[self.get_position() + 1:]) +
 			(']' if self.get_position() + 1 == len(self._tape) else ' ]'))
 
+def _unescape(in_str):
+    index = 0
+    out_str = ""
+    while index < len(in_str):
+        if in_str[index] == "\\":
+            out_str += in_str[index+1]
+            index += 2
+        else:
+            out_str += in_str[index]
+            index += 1
+    return out_str
+
 if __name__ == "__main__":
     # Get command line arguments
     command_line_args = sys.argv
@@ -108,6 +120,9 @@ if __name__ == "__main__":
     new_func_pattern = re.compile(r"^\s*@(?P<name>[a-zA-Z_]\w*)\s*\((?P<parameters>(?:\s*[a-zA-Z_]\w*\s*(?:,\s*[a-zA-Z_]\w*\s*)*)|(?:\s*))\)\s*(?P<initial_state>[a-zA-Z_]\w*)\s*(?:#.*)?$")
     exec_func_pattern = re.compile(r"^\s*(?P<initial_state>[a-zA-Z_]\w*)\s+(?P<initial_value>[01\*])\s+!(?P<function>[a-zA-Z_]\w*)\s*\((?P<parameters>(?:\s*[a-zA-Z_]\w*\s*(?:,\s*[a-zA-Z_]\w*\s*)*)|(?:\s*))\)\s+(?P<final_state>[a-zA-Z_]\w*|\*)\s*(?:#.*)?$")
     move_pattern = re.compile(r"^\s*(?P<initial_state>[a-zA-Z_]\w*)\s+(?P<initial_value>[01\*])\s+(?P<final_value>[01\*])\s+(?P<operation>[<>\*])(?:(?<!\*)(?P<count>[1-9][0-9]*)?(?::(?P<fill>[01\*]))?)?\s+(?P<final_state>[a-zA-Z_]\w*|\*)\s*(?:#.*)?$")
+    if_pattern = re.compile(r"^\s*(?P<initial_state>[a-zA-Z_]\w*)\s+(?P<initial_value>[01\*])\s+!if\s*\(\s*(?P<condition>[a-zA-Z_]\w*)\s*\)\s*(?P<true_state>[a-zA-Z_]\w*)\s*:\s*(?P<false_state>[a-zA-Z_]\w*)\s*(?:#.*)?$")
+    input_pattern = re.compile(r'^\s*(?P<initial_state>[a-zA-Z_]\w*)\s+(?P<initial_value>[01\*])\s+!input\s*\(\s*(?P<min_count>[1-9]\d*)\s*,\s*(?:(?P<max_count>[1-9]\d*),\s*)?"(?P<prompt>(?:(?<!\\)(?:\\{2})*\\"|[^"\\])*(?<!\\)(?:\\{2})*)"\s*\)\s*(?P<final_state>[a-zA-Z_]\w*|\*)\s*(?:#.*)?$')
+    print_pattern = re.compile(r'^\s*(?P<initial_state>[a-zA-Z_]\w*)\s+(?P<initial_value>[01\*])\s+!print\s*\(\s*"(?P<text>(?:(?<!\\)(?:\\{2})*\\"|[^"\\])*(?<!\\)(?:\\{2})*)"\s*\)\s*(?P<final_state>[a-zA-Z_]\w*)\s*(?:#.*)?$')
 
     for line_num, line in enumerate(script):
         # Use a 1-indexed line number
@@ -124,6 +139,10 @@ if __name__ == "__main__":
 
             # Get expression information
             name = new_func_match.group("name")
+
+            if name in ("flag", "goto", "if", "input", "print"):
+                # User tried to re-define builtin function
+                raise ValueError(f'Unable to re-define builtin function "!{name}" on line {line_num}.')
 
             if name in functions.keys():
                 # Duplicate function name
@@ -162,9 +181,13 @@ if __name__ == "__main__":
                 raise ValueError(f"Repeated expression on line {line_num}.")
 
             function = exec_func_match.group("function")
+            if function in ("if", "input", "print"):
+                raise ValueError(f'Incorrect syntax for executing builtin function "!{function}" on line {line_num}.')
+
             parameters = []
             for p in exec_func_match.group("parameters").split(','):
-                parameters.append(p.strip())
+                if p.strip() != '':
+                    parameters.append(p.strip())
 
             final_state = exec_func_match.group("final_state")
 
@@ -204,11 +227,74 @@ if __name__ == "__main__":
 
             continue
 
+        if_match = if_pattern.match(line)
+        if if_match is not None:
+            initial_state = if_match.group("initial_state")
+            initial_value = if_match.group("initial_value")
+            condition = if_match.group("condition")
+            true_state = if_match.group("true_state")
+            false_state = if_match.group("false_state")
+
+            if initial_state in (true_state, false_state):
+                # Infinite loop
+                raise ValueError(f"Infinite loop detected on line {line_num}.")
+
+            functions[curr_function]["expressions"][(initial_state, initial_value)] = \
+                {"is_function": True, "function": "if", "condition": condition, "true_state": true_state, "false_state": false_state}
+            continue
+
+        input_match = input_pattern.match(line)
+        if input_match is not None:
+            initial_state = input_match.group("initial_state")
+            initial_value = input_match.group("initial_value")
+            min_count = input_match.group("min_count")
+            max_count = input_match.group("max_count")
+            prompt = input_match.group("prompt")
+            final_state = input_match.group("final_state")
+
+            if max_count is None:
+                max_count = min_count
+            elif int(max_count) < int(min_count):
+                raise ValueError(f"The maximum input count is less than the minimum input count on line {line_num}.")
+
+            if final_state == initial_state:
+                # Infinite loop
+                raise ValueError(f"Infinite loop detected on line {line_num}.")
+
+            functions[curr_function]["expressions"][(initial_state, initial_value)] = \
+                {"is_function": True, "function": "input", "min_count": min_count, "max_count": max_count, "prompt": _unescape(prompt), "final_state": final_state}
+
+            continue
+
+        print_match = print_pattern.match(line)
+        if print_match is not None:
+            initial_state = print_match.group("initial_state")
+            initial_value = print_match.group("initial_value")
+            text = print_match.group("text")
+            final_state = print_match.group("final_state")
+
+            if final_state == initial_state:
+                # Infinite loop
+                raise ValueError(f"Infinite loop detected on line {line_num}.")
+
+            functions[curr_function]["expressions"][(initial_state, initial_value)] = \
+                {"is_function": True, "function": "print", "text": _unescape(text), "final_state": final_state}
+            continue
+
         # Invalid expression
         raise ValueError(f"Invalid expression on line {line_num}.")
 
     if "main" not in functions.keys():
         raise ValueError("No main function specified.")
+
+    '''
+    # print all expressions
+    for f in functions:
+        print(f"Name: {f}, Parameters: {functions[f]['parameters']}")
+        for e in functions[f]["expressions"]:
+            print(e, functions[f]["expressions"][e])
+    '''
+
 
     # Initialize the tape
     tape = TuringTape()
@@ -223,7 +309,7 @@ if __name__ == "__main__":
             cmd = functions[stack[-1]["name"]]["expressions"][(stack[-1]["state"], '*')]
         except KeyError:
             try:
-                cmd = functions[stack[-1]["name"]]["expressions"][(stack[-1]["state"], tape.selected)]
+                cmd = functions[stack[-1]["name"]]["expressions"][(stack[-1]["state"], str(tape.selected))]
             except KeyError:
                 # Remove top layer from the stack
                 del stack[-1]
@@ -249,6 +335,44 @@ if __name__ == "__main__":
                 except KeyError:
                     raise KeyError(f"Flag name {cmd['parameters'][0]} referenced before creation.")
                 stack[-1]["state"] = cmd["final_state"]
+
+            elif cmd["function"] == "if":
+                if cmd["condition"] not in stack[-1]["flags"].keys():
+                    raise KeyError(f"Flag name {cmd['condition']} referenced before creation.")
+                if stack[-1]["flags"][cmd["condition"]] == tape.get_position():
+                    stack[-1]["state"] = cmd["true_state"]
+                else:
+                    stack[-1]["state"] = cmd["false_state"]
+
+            elif cmd["function"] == "input":
+                # Get user input
+                user_input = input(cmd["prompt"] + ' ')
+                user_input_pattern = re.compile("^[01]{" + cmd["min_count"] + ',' + cmd["max_count"] + "}$")
+                user_input_match = user_input_pattern.match(user_input)
+
+                while not user_input_match:
+                    # Get valid user input
+                    if min_count == max_count:
+                        if min_count == '1':
+                            user_input = input(f"ERROR: Enter a 0 or a 1: ")
+                        else:
+                            user_input = input(f"ERROR: Enter exactly {cmd['min_count']} 0s and 1s: ")
+                    else:
+                        user_input = input(f"ERROR: Enter between {cmd['min_count']} and {cmd['max_count']} 0s and 1s: ")
+
+                    user_input_match = user_input_pattern.match(user_input)
+
+                # Add user input to the tape
+                for bit in user_input_match.group():
+                    tape.selected = int(bit)
+                    tape.right(1, '*')
+
+                stack[-1]["state"] = cmd["final_state"]
+
+            elif cmd["function"] == "print":
+                print(cmd["text"])
+                stack[-1]["state"] = cmd["final_state"]
+
             else:
                 # Custom function
                 if cmd["function"] not in functions.keys():
@@ -270,9 +394,10 @@ if __name__ == "__main__":
                 stack.append({"name": cmd["function"], "flags": flags, "state": functions[cmd["function"]]["initial_state"]})
 
         else:
+            # Move expression
             # Set the selected value
             if cmd["final_value"] != '*':
-                tape.selected = cmd["final_value"]
+                tape.selected = int(cmd["final_value"])
 
             if cmd["operation"] == '<':
                 tape.left(int(cmd["count"]), cmd["fill"])
